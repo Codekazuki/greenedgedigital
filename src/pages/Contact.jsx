f126034b2acc4262c726bbe3351ff80406665e4c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-09-17 11:50:16 +0100

    first update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import contactimage from "../assets/email.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "../components/contact.css";
 
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stop page reload
+    toast.success(`Message sent successfully!`);
+    e.target.reset();
+  };
+
   return (
     <section className='container'>
       <div className='contact'>
         <div className='contact-img'>
           <img className='img2' src={contactimage} alt='Contact' />
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>
             <span className='icon'>
-              <i class='ri-customer-service-2-line'></i>
+              <i className='ri-customer-service-2-line'></i>
             </span>
             Get In <span className='highlight'>Touch</span>
           </h1>
+
           <div className='input-box'>
             <label htmlFor='name'>Full Name</label>
             <input
@@ -28,6 +37,7 @@ export default function Contact() {
               placeholder='Input your name'
             />
           </div>
+
           <div className='input-box'>
             <label htmlFor='email'>Email</label>
             <input
@@ -50,10 +60,13 @@ export default function Contact() {
               placeholder='Your message'
             ></textarea>
           </div>
+
           <button className='btn-submit' type='submit'>
             Submit
           </button>
         </form>
+
+        <ToastContainer position='top-center' autoClose={3000} />
       </div>
     </section>
   );
